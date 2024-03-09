12de0ac77381d723adf5d0f650a3ae08d5c3a6b8
Saurabh Salunke <saurabhsalunke91@gmail.com>
2024-03-09 19:38:12 +0530

    Terrform

---
+++ Terraform/vpc/vpc.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "demo-sg-server" {
+  ami = "ami-0f403e3180720dd7e"
+  instance_type = "t2.micro"
+  key_name = "dep"
+#   security_groups = [ "demo_sg" ]
+    vpc_security_group_ids = [aws_security_group.demo_sg.id]
+    subnet_id = aws_subnet.dpw-public_subent_01.id
+}
+resource "aws_security_group" "demo_sg" {
+  name        = "demo_sg"
+  description = "Allow ssh inbound traffic"
+ vpc_id = aws_vpc.dpw-vpc.id
+  ingress {
+    description      = "ssh access to public"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
+  resource "aws_vpc" "dpw-vpc" {
+       cidr_block = "10.1.0.0/16"
+       tags = {
+        Name = "dpw-vpc"
+     }
+   }
+   //Create a Subnet 
+resource "aws_subnet" "dpw-public_subent_01" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "dpw-public_subent_01"
+    }
+}
+//Create a Subnet 
+resource "aws_subnet" "dpw-public_subent_02" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1b"
+    tags = {
+      Name = "dpw-public_subent_02"
+    }
+}
+//Creating an Internet Gateway 
+resource "aws_internet_gateway" "dpw-igw" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    tags = {
+      Name = "dpw-igw"
+    }
+}
+// Create a route table 
+resource "aws_route_table" "dpw-public-rt" {
+    vpc_id = aws_vpc.dpw-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dpw-igw.id
+    }
+    tags = {
+      Name = "dpw-public-rt"
+    }
+}
+// Associate subnet with route table
+
+resource "aws_route_table_association" "dpw-rta-public-subent-1" {
+    subnet_id = aws_subnet.dpw-public_subent_01.id
+    route_table_id = aws_route_table.dpw-public-rt.id
+}
+// Associate subnet with route table
+
+resource "aws_route_table_association" "dpw-rta-public-subent-2" {
+    subnet_id = aws_subnet.dpw-public_subent_02.id
+    route_table_id = aws_route_table.dpw-public-rt.id
+}
